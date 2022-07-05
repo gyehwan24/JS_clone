dd2fb6fd91a257a1abf1042c2944e8c8086b942a
Gyehwan Baek <b99hwan@gmail.com>
2022-07-05 17:09:26 +0900

    add todo-list

---
+++ JS/todo.js
@@ -0,0 +1,21 @@
+const toDoForm = document.querySelector("#todo-form");
+const toDoInput = toDoForm.querySelector("input");
+// == document.querySelector("#todo-form input");
+const toDoList = document.querySelector("#todo-list");
+
+function handleToDoSubmit(event){
+    event.preventDefault();
+    const newToDo = toDoInput.value;
+    toDoInput.value = "";
+    paintToDo(newToDo);
+}
+
+function paintToDo(newToDo){
+    const li = document.createElement("li");
+    const span = document.createElement("span");
+    li.appendChild(span);
+    span.innerText = newToDo;
+    toDoList.appendChild(li);
+}
+
+toDoForm.addEventListener("submit", handleToDoSubmit);
